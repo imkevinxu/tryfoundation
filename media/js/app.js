9425521895d152ca6fd2620c2d94fd744e6a497d
Kevin Xu <kevin@imkevinxu.com>
2013-04-25 07:29:58 -0700

    chrome frame on top of iframe

--- media/js/app.js
+++ media/js/app.js
@@ -2,11 +2,44 @@
 
   /* Javascript masterminded by Kevin Xu <kevin@imkevinxu.com> */
 
+  // Adjusts height for IDE view
   var winHeight = $(window).height();
   $('#editor').height(winHeight - 45);
-  $('#preview').height((winHeight - 45)/2);
+  $('#preview-wrapper').height((winHeight - 45)/2);
+  $('#preview').height((winHeight - 45)/2 - $('#chrome-frame').height());
 
+  /* -----------------------------------------
+     ACE EDITOR SETTINGS
+  ----------------------------------------- */
 
+  if ($("#editor").length) {
+    var editor = ace.edit("editor");
+    var session = editor.getSession();
+    var iframe = $('#preview')[0];
+
+    editor.setTheme("ace/theme/tomorrow_night_eighties");
+    session.setMode("ace/mode/html");
+    session.setUseWrapMode(true);
+    editor.setFontSize(20);
+
+    var updatePreview = function() {
+        iframe.contentWindow.document.open('text/html', 'replace');
+        iframe.contentWindow.document.write(session.getValue());
+        iframe.contentWindow.document.close();
+        var iframeContents = $('#preview').contents();
+        iframeContents.find('head').html('<link rel="stylesheet" href="/media/css/foundation.min.css">');
+        iframeContents.find('body').css('padding', '15px 20px 0px');
+        iframeContents.find('a').each(function() {
+          $(this).attr("target", "_blank");
+          if ($(this).attr('href') && $(this).attr('href').indexOf("http://") < 0) {
+            $(this).attr('href', "http://" + $(this).attr('href'));
+          }
+        });
+    }
+
+    updatePreview();
+    session.on("change", updatePreview);
+  }
 
   /* -----------------------------------------
      ZURB FOUNDATION INITIALIZATION
@@ -15,7 +48,7 @@
   $(document).foundation();
 
   // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
-  if (Modernizr.touch) {
+  if (window.Modernizr.touch) {
     $(window).load(function () {
       setTimeout(function () {
         window.scrollTo(0, 1);
