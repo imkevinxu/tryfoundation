0a3ceb2332b99b60e683de38b641132238cd92a1
Kevin Xu <kevin@imkevinxu.com>
2013-04-25 11:13:13 -0700

    lessons using model logic

--- librarian/models.py
+++ librarian/models.py
@@ -18,6 +18,23 @@ class Lesson(Base):
     title = models.CharField(max_length=255)
     slug  = models.SlugField(max_length=255)
 
+    def _code_partial(self):
+        return 'code/%s.html' % self.slug
+    def _lesson_partial(self):
+        return 'lesson/%s.html' % self.slug
+    def _example_partial(self):
+        return 'example/%s.html' % self.slug
+    def _documentation_partial(self):
+        return 'documentation/%s.html' % self.slug
+    def _quiz_partial(self):
+        return 'quiz/%s.html' % self.slug
+
+    code_partial          = property(_code_partial)
+    lesson_partial        = property(_lesson_partial)
+    example_partial       = property(_example_partial)
+    documentation_partial = property(_documentation_partial)
+    quiz_partial          = property(_quiz_partial)
+
     def save(self, *args, **kwargs):
         if not self.id:
             # Newly created object, so set slug
