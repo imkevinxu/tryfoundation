2f238a03640518d9691f4bedeffa9b6b0e76213e
Kevin Xu <kevin@imkevinxu.com>
2013-04-25 07:47:19 -0700

    working on lesson content

--- media/js/app.js
+++ media/js/app.js
@@ -2,17 +2,23 @@
 
   /* Javascript masterminded by Kevin Xu <kevin@imkevinxu.com> */
 
-  // Adjusts height for IDE view
-  var winHeight = $(window).height();
-  $('#editor').height(winHeight - 45);
-  $('#preview-wrapper').height((winHeight - 45)/2);
-  $('#preview').height((winHeight - 45)/2 - $('#chrome-frame').height());
+
+
+
 
   /* -----------------------------------------
      ACE EDITOR SETTINGS
   ----------------------------------------- */
 
   if ($("#editor").length) {
+    // Adjusts height for IDE view
+    $(window).resize(function() {
+      var winHeight = $(window).height();
+      $('#editor').height(winHeight - 45);
+      $('#preview-wrapper').height((winHeight - 45)/2);
+      $('#preview').height((winHeight - 45)/2 - $('#chrome-frame').height() + 1);
+    });
+
     var editor = ace.edit("editor");
     var session = editor.getSession();
     var iframe = $('#preview')[0];
@@ -28,7 +34,7 @@
         iframe.contentWindow.document.close();
         var iframeContents = $('#preview').contents();
         iframeContents.find('head').html('<link rel="stylesheet" href="/media/css/foundation.min.css">');
-        iframeContents.find('body').css('padding', '15px 20px 0px');
+        iframeContents.find('body').css('padding', '10px 20px 0px');
         iframeContents.find('a').each(function() {
           $(this).attr("target", "_blank");
           if ($(this).attr('href') && $(this).attr('href').indexOf("http://") < 0) {
